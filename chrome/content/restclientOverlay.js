f99bcf1cc675912c492fd967993d1a9c469e368f
Chao ZHOU <chao@zhou.fr>
2011-01-15 18:06:27 +0800

    Merge with kazuhiro's repo

--- chrome/content/restclientOverlay.js
+++
@@ -1,7 +0,0 @@
-function loadRestClient() {
-    var theTab          = gBrowser.addTab('chrome://restclient/content/restclient.xul');
-    theTab.label        = "REST Client";
-    gBrowser.selectedTab = theTab;
-    var func = function () { gBrowser.setIcon(theTab, "chrome://restclient/skin/logo16.png"); };
-    setTimeout(func, 500);
-}
--- upload.cmd
+++
@@ -1,5 +0,0 @@
-pscp restclient.xpi bitibeloved@ut-insa.com:/home/bitibeloved/argos.ut-insa.com/restclient/restclient.xpi
-pscp update.rdf bitibeloved@ut-insa.com:/home/bitibeloved/argos.ut-insa.com/restclient/update.rdf
-REM pscp restclient.xpi zhouchao@shell.sourceforge.net:/home/groups/a/ar/argos-viewpoint/htdocs/restclient/restclient.xpi
-REM pscp update.rdf zhouchao@shell.sourceforge.net:/home/groups/a/ar/argos-viewpoint/htdocs/restclient/update.rdf
-del restclient.xpi

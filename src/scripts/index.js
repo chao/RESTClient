c9cdb60c1da6af2772fc3718c8959c0bece0c5b9
Chao ZHOU <chao@zhou.fr>
2017-10-11 14:15:00 +0800

    Added favorite urls;

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -25,17 +25,58 @@ $(function () {
         $('#request-method').val(method);
     });
     // Load favorite urls from storage
+    $('.btn-toggle-favorite-url').prop('disabled', true);
     browser.storage.local.get('request-urls').then(
         (urls) => {
             if(typeof urls == 'object' && urls.length > 0)
             {
-
+                window.favoriteUrls = urls;
             }
             else
             {
-
+                window.favoriteUrls = [];
             }
+            $(document).trigger('init-favorite-url-dropdown-items');
+            $('.btn-toggle-favorite-url').prop('disabled', false);
         }
     );
+
+    $('.btn-toggle-favorite-url').on('click', function(){
+        var url = $('#request-url').val();
+        if(url == '')
+        {
+            return false;
+        }
+        var idx = _.indexOf(favoriteUrls, url);
+        if(idx >= 0)
+        {
+            _.remove(favoriteUrls, function(item) {
+                return item == url;
+            });
+        }
+        else
+        {
+            favoriteUrls.push(url);
+        }
+        $(document).trigger('init-favorite-url-dropdown-items');
+        $('.btn-toggle-favorite-url i').toggleClass('fa-star-o');
+        $('.btn-toggle-favorite-url i').toggleClass('fa-star');
+    });
+
+    $(document).on('init-favorite-url-dropdown-items', function(){
+        $('#request-urls-dropdown').empty();
+        if(typeof favoriteUrls == 'object' && favoriteUrls.length > 0)
+        {
+            _.forEach(favoriteUrls, function(url) {
+                var el = $('<a class="dropdown-item" href="#"></a>').text(url);
+                $('#request-urls-dropdown').append(el);
+            });
+        }
+        else
+        {
+            var li = $('<a class="dropdown-item disabled" href="#">No favorite URLs</a>');
+            $('#request-urls-dropdown').append(li);
+        }
+    });
     console.log('Hello jQuery');
 });

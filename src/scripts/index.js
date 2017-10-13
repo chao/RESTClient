5ce73f3bb964ae92222a2cfd519718548aab642e
Chao ZHOU <chao@zhou.fr>
2017-10-13 16:29:06 +0800

    Added scroll to top button.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -380,6 +380,18 @@ $(function () {
         {
             $('.' + target).toggle();
         }
+    });
 
-    })
+    /******************** Back to Top *************************/
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $('#scroll').fadeIn();
+        }else{
+            $('#scroll').fadeOut();
+        }
+    });
+    $('#scroll').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
 });

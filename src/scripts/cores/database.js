0f8e5287fc11d51d7044a0f0d513d93843f072cc
Chao ZHOU <chao@zhou.fr>
2017-11-24 15:39:27 +0800

    Import favorite requests to IndexedDB

---
+++ src/scripts/cores/database.js
@@ -0,0 +1,118 @@
+var Database = {
+    DB_VERSION: 1,
+    DB_NAME: 'restclient',
+
+    _db: null,
+    _requests: [],
+    db() {
+        return this._db;
+    },
+
+    get requests() {
+        return this._requests;
+    },
+
+    set requests(value) {
+        this._requests = value;
+    },
+
+    getRequest(requestId) {
+        let request = this.requests.filter(f => f.id === requestId);
+        if (request === undefined) {
+            return undefined;
+        }
+        return Object.assign({}, request);
+    },
+
+    async init() {
+        console.log(`[RESTClient][database.js]: initing database...`);
+        if (this._db)
+            return;
+        let { storage } = await browser.storage.local.get({ storage: 'persistent' });
+        console.log(`[RESTClient][database.js]: opening database in ${storage} storage`);
+        let options = { version: this.DB_VERSION };
+        if (storage === 'persistent') {
+            options.storage = 'persistent';
+        }
+        let opener = indexedDB.open(this.DB_NAME, options);
+
+        opener.onupgradeneeded = (event) => this._upgradeSchema(event);
+        this._db = await this._requestPromise(opener);
+        await this.loadRequests();
+        console.log(`[RESTClient][database.js]: opened database with ${this._requests.length} requests`);
+    },
+
+    _upgradeSchema(event) {
+        console.log(`[RESTClient][database.js]: upgrade from version ${event.oldVersion}`);
+        let { result: db, transaction: tx } = event.target;
+        let requests;
+        switch (event.oldVersion) {
+            case 0:
+                requests = db.createObjectStore("requests", {
+                    keyPath: "id", autoIncrement: true
+                });
+
+                requests.createIndex("idxRequestName", "request_name");
+                requests.createIndex("idxTagName", "tags", { multiEntry: true });
+        }
+    },
+
+    // Note: this is resolved after the transaction is finished(!!!) mb1193394
+    _requestPromise(req) {
+        return new Promise((resolve, reject) => {
+            req.onsuccess = (event) => resolve(event.target.result);
+            req.onerror = (event) => reject(event.target.error);
+        });
+    },
+
+    // Note: this is resolved after the transaction is finished(!)
+    _transactionPromise(tx) {
+        return new Promise((resolve, reject) => {
+            let oncomplete = tx.oncomplete;
+            let onerror = tx.onerror;
+            tx.oncomplete = () => { resolve(); if (oncomplete) oncomplete(); };
+            tx.onerror = () => { reject(); if (onerror) onerror(); };
+        });
+    },
+
+    async loadRequests() {
+        let tx = this._db.transaction(['requests']);
+        let request = tx.objectStore('requests').getAll();
+        let requests = await this._requestPromise(request);
+        console.log(`[RESTClient][database.js]:${requests.length} requests in database`);
+
+        if (requests.length === 0) {
+            console.log(`[RESTClient][database.js]: the database looks empty`);
+            ({ requests } = await browser.storage.local.get({ requests: [] }));
+            console.log(`[RESTClient][database.js]: ${requests.length} requests found in local storage`);
+            if (requests.length === 0) {
+                ({ requests } = await browser.storage.sync.get({ requests: [] }));
+                console.log(`[RESTClient][database.js]: ${requests.length} requests found in sync storage`);
+            }
+            this._requests = requests;
+        }
+
+        requests.sort((a, b) => a.rowIndex - b.rowIndex);
+        this._requests = requests;
+    },
+
+    async importRequests(items) {
+        if (this._db === null) {
+            return;
+        }
+        let tx = this._db.transaction(['requests'], 'readwrite');
+        let imported = 0;
+        for (let name in items) {
+            let item = items[name];
+            item.name = name;
+            item.tags = [];
+            tx.objectStore('requests').put(item);
+            imported++;
+        }
+        await this._transactionPromise(tx);
+        console.log(`[RESTClient][database.js]: ${imported} requests imported.`);
+    },
+}
+Database.init().then(function(){
+    console.log('database inited');
+});
--- src/scripts/cores/favorite.js
+++ src/scripts/cores/favorite.js
@@ -1,42 +1,75 @@
-var dropzone;
-var onFavoriteFileDropped = function(e) {
-    e.stopPropagation();
-    e.preventDefault();
+let Favorite = {
+    dropzone: null,
+    favoriteReader: null,
 
-    var dt = e.dataTransfer;
-    var files = dt.files;
-    console.log(files);
-    var filetypes = [];
-    var filesizes = [];
-    _.each(files, function(file) {
-        if (file.type != "application/json")
-        {
-            filetypes.push(file.name);
-        }
-        if (file.size > 5 * 1024 * 1024) {
-            filesizes.push(file.name);
-        }
-    });
-    var errors = '';
-    if (filesizes.length > 0)
-    {
-        var isOrAre = filesizes.length == 1 ? ' is' : ' are';
-        errors += "File: " + filesizes.join(', ') + isOrAre + " too large. ";        
-    }
-    if (filetypes.length > 0) {
-        var isOrAre = filetypes.length == 1 ? ' is' : ' are';
-        errors = errors === '' ? errors : errors + '<br />';
-        errors += "File: " + filetypes.join(', ') + isOrAre + " not JSON format. ";
-    }
-    if(errors !== '')
-    {
+    _doNothing(e) {
+        e.stopPropagation(); 
+        e.preventDefault();
+    },
+
+    _fileDropped(e) {
+        e.stopPropagation();
+        e.preventDefault();
+
+        var dt = e.dataTransfer;
+        var files = dt.files;
+        console.log(files);
+        var filetypes = [];
+        var filesizes = [];
+        _.each(files, function (file) {
+            if (file.type != "application/json") {
+                filetypes.push(file.name);
+            }
+            if (file.size > 5 * 1024 * 1024) {
+                filesizes.push(file.name);
+            }
+        });
         $('#modal-file-import').modal('hide');
-        toastr.error(errors, 'Cannot import favorite files', { "timeOut": "15000" });
-        return false;
-    }
-}
+        var errors = '';
+        if (filesizes.length > 0) {
+            var isOrAre = filesizes.length == 1 ? ' is' : ' are';
+            errors += "File: " + filesizes.join(', ') + isOrAre + " too large. ";
+        }
+        if (filetypes.length > 0) {
+            var isOrAre = filetypes.length == 1 ? ' is' : ' are';
+            errors = errors === '' ? errors : errors + '<br />';
+            errors += "File: " + filetypes.join(', ') + isOrAre + " not JSON format. ";
+        }
+        if (errors !== '') {
+            toastr.error(errors, 'Cannot import favorite files', { "timeOut": "15000" });
+            return false;
+        }
+        _.each(files, function (file) {
+            toastr.info('Start to import...', file.name);
+            Favorite.favoriteReader.onloadend = (function (file) {
+                return function (e) {
+                    console.log(e);
+                    console.log(file);
+                    var content = this.result;
+                    try{
+                        var requests = JSON.parse(content);
+                        Database.importRequests(requests).then(function(e){
+                            toastr.success("The file has been successfully imported", file.name);
+                        });
+                    }
+                    catch(e)
+                    {
+                        toastr.error('Format error', file.name);
+                    }
+                };
+            })(file);
+            Favorite.favoriteReader.readAsText(file);
+        });
+    },
 
-dropzone = document.getElementById("dropzone-import");
-dropzone.addEventListener("dragend", function (e) { e.stopPropagation(); e.preventDefault(); }, false);
-dropzone.addEventListener("dragover", function (e) { e.stopPropagation(); e.preventDefault(); }, false);
-dropzone.addEventListener("drop", onFavoriteFileDropped, false);
+
+    init() {
+        this.favoriteReader = new FileReader();
+
+        this.dropzone = document.getElementById("dropzone-import");
+        this.dropzone.addEventListener("dragend", this._doNothing, false);
+        this.dropzone.addEventListener("dragover", this._doNothing, false);
+        this.dropzone.addEventListener("drop", this._fileDropped, false);
+    },
+}
+Favorite.init();

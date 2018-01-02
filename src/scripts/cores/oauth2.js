d172b573372e476b2898620ce4c6c8be9f74bb34
Chao ZHOU <chao@zhou.fr>
2018-01-02 17:12:40 +0800

    Added CSP safe mustache.js

--- src/scripts/cores/oauth2.js
+++ src/scripts/cores/oauth2.js
@@ -24,14 +24,84 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ***** END LICENSE BLOCK ***** */
 
 $(function () {
+  
+  $(document).on('oauth2-grant-type-changed', function(){
+    var type = $('[name="oauth2-grant-type"]:checked').val();
+    console.log('[oauth2.js] grant type changed', type);
+    $('#oauth2-authorization .form-group:not([data-' + type + '])').hide();
+    $('#oauth2-authorization .form-group[data-' + type + ']').show();
+  });
+  // When toggle oauth2 grant type
+  $(document).on('input change click', '[name="oauth2-grant-type"]', function () {
+    $(document).trigger('oauth2-grant-type-changed');
+  });
 
-    // When toggle oauth2 grant type
-    $(document).on('input change click', '[name="oauth2-grant-type"]', function () {
-        var type = $('[name="oauth2-grant-type"]:checked').val();
-        console.log('[oauth2.js] grant type changed', type);
-        $('#oauth2-authorization .form-group:not([data-' + type + '])').hide();
-        $('#oauth2-authorization .form-group[data-' + type + ']').show();
-    }).trigger('change');
+  // When toggle oauth2 state auto mode
+  $(document).on('click', '#oauth2-state-auto', function () {
+    var checked = $(this).is(':checked');
+    if (checked) {
+      $('#oauth2-state').val('').attr('readonly', 'true');
+    }
+    else {
+      $('#oauth2-state').val(Misc.random(16)).removeAttr('readonly');
+    }
+  });
 
+  $(document).on('submit', '#form-oauth2', function (e) {
+    e.preventDefault();
+    $('#modal-oauth2 .has-error').removeClass('has-error');
 
+    var params = {
+      'grant_type': $('[name="oauth2-grant-type"]:checked').val(),
+      'client_id': $('#oauth2-client-id').val(),
+      'client_secret': $('#oauth2-client-secret').val(),
+      'scope': $('#oauth2-scope').val(),
+      'state': $('#oauth2-state-auto').is(':checked') ? true : $('#oauth2-state').val(),
+      'request_method': $('[name="oauth2-request-method"]:checked').val(),
+      'authorization_endpoint': $('#oauth2-authorization-endpoint').val(),
+      'token_endpoint': $('#oauth2-token-endpoint').val(),
+      'redirect_endpoint': $('#oauth2-redirect-endpoint').val(),
+    };
+
+    var error = false;
+    if (params.client_id == '') {
+      $('#oauth2-client-id').parents('.form-group').addClass('has-danger').find('.helper').show();
+      error = true;
+    }
+    if (params.client_secret == '') {
+      $('#oauth2-client-secret').parents('.form-group').addClass('has-danger').find('.helper').show();
+      error = true;
+    }
+    if ($('#oauth2-authorization-endpoint').is(':visible') && params.authorization_endpoint == '') {
+      $('#oauth2-authorization-endpoint').parents('.form-group').addClass('has-danger').find('.helper').show();
+      error = true;
+    }
+    if (params.token_endpoint == '') {
+      $('#oauth2-token-endpoint').parents('.form-group').addClass('has-danger').find('.helper').show();
+      error = true;
+    }
+    if (error) {
+      return false;
+    }
+
+    if ($('#save-oauth2').is(':checked')) {
+      storage.set({ ['oauth2']: params }).then(() => {
+        console.log('[oauth2.js] storage saved!');
+      });
+    }
+    console.log('[oauth2.js] oauth2-form submit', params);
+    $('.authentication-mode').removeClass('active');
+    $('.authentication-mode[data-mode="oauth20"]')
+      .addClass('active')
+      .data('params', params);
+    $('#modal-oauth2').modal('hide');
+  });
+
+  // When OAuth2 modal is opened
+  $(document).on('show.bs.modal', '#modal-oauth2', function(){
+    $('#modal-oauth2 .has-error').removeClass('has-error');
+    $('#form-oauth2')[0].reset();
+
+    $(document).trigger('oauth2-grant-type-changed');
+  });
 });

487b3688d2b0be946d7b4b4976e9e78fd017651b
Chao ZHOU <chao@zhou.fr>
2018-01-08 14:39:32 +0800

    Added private browsing mode detection and warning.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -23,98 +23,101 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * ***** END LICENSE BLOCK ***** */
 
-window.ext = require( "./utils/ext" );
-window.storage = require( "./utils/storage" );
+window.ext = require("./utils/ext");
+window.storage = require("./utils/storage");
 window.curlconverter = require('curlconverter');
+window.currentTabInfo = false;
+ext.tabs.getCurrent().then(function (tabInfo) {
+  console.log(`[index.js] getCurrent`, tabInfo);
+  window.currentTabInfo = tabInfo;
+  if (tabInfo.incognito)
+  {
+    toastr.warning('You are using private browsing mode, you will not be able to use "favorites" function.', 'Private Browsing', {
+      "positionClass": "toast-bottom-full-width",
+      "closeButton": true,
+      "timeOut": 0,
+      "extendedTimeOut": 0
+    });
+  }
+}, function (error) {
+  console.log(`[index.js] Error: ${error}`);
+});
+
 $(function () {
-    window.favoriteHeaders = [];
-    window.favoriteUrls = [];
-    $(document).on('click', '[data-toggle="development"]', function(){
-        toastr.error('This function is still under development.', 'Not ready yet!');
-    });
-    /**************************** Init Toastr ********************************/
-    toastr.options = {
-        "closeButton": true,
-        "debug": false,
-        "newestOnTop": true,
-        "progressBar": true,
-        "positionClass": "toast-top-right",
-        "preventDuplicates": true,
-        "onclick": null,
-        "showDuration": "300",
-        "hideDuration": "1000",
-        "timeOut": "5000",
-        "extendedTimeOut": "1000",
-        "showEasing": "swing",
-        "hideEasing": "linear",
-        "showMethod": "fadeIn",
-        "hideMethod": "fadeOut"
+  window.favoriteHeaders = [];
+  window.favoriteUrls = [];
+  $(document).on('click', '[data-toggle="development"]', function () {
+    toastr.error('This function is still under development.', 'Not ready yet!');
+  });
+  /**************************** Init Toastr ********************************/
+  toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": true,
+    "progressBar": true,
+    "positionClass": "toast-top-right",
+    "preventDuplicates": true,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+  }
+
+  /******************** Send Button ****************/
+  $('.btn-send-request').prop('disabled', true);
+  $(document).on('keyup input change paste', '#request-method, #request-url', function () {
+    console.log('url changed');
+    var method = $('#request-method').val();
+    var url = $('#request-url').val();
+    var isUrl = urlHelper.is_web_iri(url);
+    // console.log(isUrl);
+    // console.log(method != '' && typeof isUrl != 'undefined');
+    if (method != '' && typeof isUrl != 'undefined') {
+      $('.btn-send-request').prop('disabled', false);
     }
-    
-    /******************** Send Button ****************/
-    $('.btn-send-request').prop('disabled', true);
-    $(document).on('keyup input change paste', '#request-method, #request-url', function(){
-        console.log('url changed');
-        var method = $('#request-method').val();
-        var url = $('#request-url').val();
-        var isUrl = urlHelper.is_web_iri(url);
-        // console.log(isUrl);
-        // console.log(method != '' && typeof isUrl != 'undefined');
-        if(method != '' && typeof isUrl != 'undefined')
-        {
-            $('.btn-send-request').prop('disabled', false);
-        }
-        else
-        {
-            $('.btn-send-request').prop('disabled', true);
-        }
-    }).trigger('change');
+    else {
+      $('.btn-send-request').prop('disabled', true);
+    }
+  }).trigger('change');
 
-    /*************************** Execute request ******************************/
-    $('.request-form').on('submit', function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        if($('.btn-send-request').prop('disabled'))
-        {
-            return false;
-        }
-        var request;
-        try{
-            request = Request.getProcessed();
-        }
-        catch(e)
-        {
-            toastr.error(e);
-            return false;
-        }
+  /*************************** Execute request ******************************/
+  $('.request-form').on('submit', function (e) {
+    e.preventDefault();
+    e.stopPropagation();
+    if ($('.btn-send-request').prop('disabled')) {
+      return false;
+    }
+    var request;
+    try {
+      request = Request.getProcessed();
+    }
+    catch (e) {
+      toastr.error(e);
+      return false;
+    }
 
-        $('#response-headers ol').empty();
-        cmResponseBody.getDoc().setValue('');
-        cmResponseBodyPreview.getDoc().setValue('');
-        $('#tab-response-preview .CodeMirror').hide();
-        $('#iframe-response').show();
-        $('.response-container a.preview[data-toggle="tab"]').hide();
+    $('#response-headers ol').empty();
+    cmResponseBody.getDoc().setValue('');
+    cmResponseBodyPreview.getDoc().setValue('');
+    $('#tab-response-preview .CodeMirror').hide();
+    $('#iframe-response').show();
+    $('.response-container a.preview[data-toggle="tab"]').hide();
 
-        ext.tabs.getCurrent().then(function (tabInfo) {
-            console.log(`[index.js][request-form submit] getCurrent`, tabInfo);
-        }, function(error){
-            console.log(`[index.js][request-form submit] Error: ${error}`);
-        });
-
-        ext.runtime.sendMessage({
-            action: "execute-http-request",
-            target: "background",
-            data: request
-        });
-
-        $('.current-request-basic').html(request.method + ' ' + request.url);
-        $(document).trigger("show-fullscreen");
-
-        
-        
-
-        $(document).trigger('request-updated', [request]);
+    ext.runtime.sendMessage({
+      action: "execute-http-request",
+      target: "background",
+      data: request
     });
 
-    
+    $('.current-request-basic').html(request.method + ' ' + request.url);
+    $(document).trigger("show-fullscreen");
+    $(document).trigger('request-updated', [request]);
+  });
+
+
 });
